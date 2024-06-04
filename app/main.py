520e5de618744f7e75f130b2b7c6cb2d365c4174
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-06-04 16:10:04 +0000

    feat: global constant

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+MAX_LINES = 3 
+
 def deposit():
     while True:
         amount = input ("What would you like to deposit? $")
@@ -11,4 +13,7 @@ def deposit():
             print("Enter a number")
     return amount
 
-deposit()
+def main():
+    balance = deposit()
+
+main()
