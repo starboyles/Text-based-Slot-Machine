80c0e330fb2ab4c98c2cca2c9cf9c2c71bd0c281
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-06-04 14:46:47 +0000

    checking validity of amount deposited

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 def deposit():
     while true:
-        amount = input ("What would you like to deposit?")
+        amount = input ("What would you like to deposit? $")
+        if amount.isdigit():
+            amount  = int(amount)
     
